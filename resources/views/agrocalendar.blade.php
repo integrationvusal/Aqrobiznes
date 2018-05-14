f649b884c44f59524b984664eeb353f908059888
Sharifov <sharifov@programmer.net>
2018-05-14 11:13:57 +0400

    Fix bug agrocalendar

--- resources/views/agrocalendar.blade.php
+++ resources/views/agrocalendar.blade.php
@@ -10,24 +10,23 @@
                 <span>{{$title}}</span>
             </div>
             <div class="clear"></div>           
-            <div class="row">   
-                <div class="col-sm-12">
-                    <table class="table table-striped table-bordered">
-						<tr>
-							<th>Tarix</th>
-							<th>Görülən işlər</th>
-						</tr>
-						@foreach($agrocalendar as $agro)
-							<tr>
-								<td width="32%">{{Helper::df($agro['date_from'])}} - {{Helper::df($agro['date_to'])}}</td>
-								<td>
-									{{$agro['title']}}
-								</td>
-							</tr>	
-						@endforeach
-					</table>
-                </div>
-            </div>              
+			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+				@foreach($agrocalendar as $agro)
+				  <div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="heading_{{$loop->index}}">
+					  <h4 class="panel-title">
+						<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse_{{$loop->index}}" aria-expanded="false" aria-controls="collapse_{{$loop->index}}">
+						  {{$agro['title']}}
+						</a>
+						<i class="fa fa-plus fa-action"></i>
+					  </h4>
+					</div>
+					<div id="collapse_{{$loop->index}}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading_{{$loop->index}}">
+					  <div class="panel-body">{!!$agro['full']!!}</div>
+					</div>
+				  </div>
+			   @endforeach
+			</div>             
         </div>
     </section>
 
